53fc74664db093aae4af9851d75a5efd24fd8550
Zandag3007 <awh.daemen@gmail.com>
2022-08-10 11:36:27 +0200

    Updated typescript code in challenge 4 & 5

--- JS-exercises/index.ts
+++ JS-exercises/index.ts
@@ -34,14 +34,15 @@
 //              Challenge 4
 // return an array of string (only min 3 char)
 // the array must include length, first char, last char, middle char(s), position of repeated character ("@ index #")
-// documentation link: https://edabit.com/challenge/5bW3TmPnae2FNvusk
-
+//
 // const allAboutStrings = (x: string): string[] => {
-//     let stringLength: number = x.length;
-//     let stringFirstChar: string = x.charAt(0);
-//     let stringLastChar: string = x.charAt(stringLength-1);
+//     let stringLength = x.length;
+//     let stringFirstChar = x.charAt(0);
+//     let stringLastChar = x.charAt(stringLength - 1);
+//
 //     let middleChar = (): string => {
-//         let char = (stringLength/2) - 1;
+//         let char = (stringLength / 2) - 1;
+//
 //         if ((stringLength % 2) === 0) {
 //             return x.charAt(char) + x.charAt(char + 1);
 //         }
@@ -49,35 +50,59 @@
 //             return x.charAt(Math.ceil(char));
 //         }
 //     }
+//
 //     let indexNr = (): string => {
-//         let charTwo: string = x.charAt(1);
-//         for (let i = 2; i <= stringLength; i++) {
-//             let currentChar = x.charAt(i);
-//             if (currentChar === charTwo) {
-//                 return `@ index ${i}`;
-//             }
-//         }
-//         return "not found";
+//         let lastInswz = x.lastIndexOf(x.charAt(1));
+//
+//         return lastInswz !== 1 ? `@ index ${lastInswz}` : 'not found';
+//
+//         // if (lastInswz !== 1) {
+//         //     return `@ index ${lastInswz}`;
+//         // } else {
+//         //     return 'not found';
+//         // }
+//         //
+//         // for (let i = 2; i <= stringLength; i++) {
+//         //     let currentChar = x.charAt(i);
+//         //
+//         //     if (currentChar === charTwo) {
+//         //         return `@ index ${i}`;
+//         //     }
+//         // }
+//         // return "not found";
 //     }
 //     return [String(stringLength), stringFirstChar, stringLastChar, middleChar(), indexNr()];
 // }
-// console.log(allAboutStrings("test"));
+// console.log(allAboutStrings("testee"));
 
 //              Challenge 5
 // return the "non-nested" number of elements in an array
 // documentation link = https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+//
 
-let getLength: any = (arrayItems: any) => {
+let ourNumber: number = 1;
+let ourNumberArray: number[] = [2, 3];
+
+type ourNewType = (number | number[])[];
+
+let ourArray: ourNewType = [ourNumber, ourNumberArray];
+
+//
+// @ts-ignore
+let theLength = ourArray.flat(Infinity).length;
+console.log(theLength);
+
+let getLength = (arrayItems: ourNewType): number => {
+    // @ts-ignore
     return arrayItems.flat(Infinity).length;
 }
-console.log(getLength([1, [2, 3]]));
+console.log(getLength(ourArray));
 
 //              Challenge 6
 // return spotlight sum of numbers in array
 // the result needs to be returned in the form of an array
 // the spotlight sum is defined as the sum of all numbers surrounding and including the number in the array
-
-let spotlightMap: any = (array: number) => {
-
-    return array;
-}
+//
+// let spotlightMap: any = (array: number) => {
+//     return array;
+// }
